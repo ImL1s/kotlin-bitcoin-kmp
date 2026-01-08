6ccfe8c9f3d0d9a9a6159d9debf8e10c506fcc7b
yami <Yami-RND@skvip.org>
2026-01-08 12:54:13 +0800

    fix(ci): comprehensive secp module substitutions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,6 +26,9 @@ val secpPath = listOf("../kotlin-secp256k1-kmp", "./kotlin-secp256k1-kmp")
 if (secpPath != null) {
     includeBuild(secpPath) {
         dependencySubstitution {
+            substitute(module("fr.acinq.secp256k1:secp256k1-kmp")).using(project(":"))
+            substitute(module("fr.acinq.secp256k1:secp256k1-kmp-jni-common")).using(project(":jni"))
+            substitute(module("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-extract")).using(project(":jni:jvm"))
             substitute(module("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-linux")).using(project(":jni:jvm:linux"))
             substitute(module("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-darwin")).using(project(":jni:jvm:darwin"))
             substitute(module("fr.acinq.secp256k1:secp256k1-kmp-jni-jvm-mingw")).using(project(":jni:jvm:mingw"))
